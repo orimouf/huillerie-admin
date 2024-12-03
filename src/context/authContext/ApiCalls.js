46e4be13727fcae8a5b82709b755fb81b669dc24
Amir Harrouche <orim.ouf@gmail.com>
2024-12-03 15:34:18 +0100

    update 03-12-2024

--- src/context/authContext/ApiCalls.js
+++ src/context/authContext/ApiCalls.js
@@ -10,7 +10,7 @@ export const login = async (user, dispatch) => {
     dispatch(loginStart());
     try {
         const res = await axios.post("/api/auth/login", user)
-        console.log(res.data);
+        console.log(res);
         
         res.data.isAdmin && dispatch(loginSuccess(res.data))
         msgLogin.style.display = "none"
