83502ac0e605c1c4634555e30b25e2583e92a02f
Amir Harrouche <orim.ouf@gmail.com>
2024-12-05 11:56:15 +0100

    update 05-12-2024

--- src/context/authContext/ApiCalls.js
+++ src/context/authContext/ApiCalls.js
@@ -1,21 +1,13 @@
-import axios from "axios";
 import Instance from '../../context/Instance'
 import { loginFailure, loginStart, loginSuccess, logout } from "./AuthActions";
 
 export const login = async (user, dispatch) => {
-    console.log("-------------");
-    console.log(user);
     let msgLogin = document.getElementsByClassName("msgLogin")[0]
     let textLogin = document.getElementsByClassName("textLogin")[0]
     let loadingLogin = document.getElementsByClassName("icnSpinner")[0]
     dispatch(loginStart());
     try {
         const res = await Instance.post("/auth/login/", user)
-        
-        console.log("---------------------");
-        console.log(res);
-        console.log(res.data);
-
         res.data.isAdmin && dispatch(loginSuccess(res.data))
         msgLogin.style.display = "none"
     } catch (err) {
